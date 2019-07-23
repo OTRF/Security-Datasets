268325e9b00a773ec698b8e5f5b8907bfc59dc24
Jonathan Johnson <johnson.s.jonathan@gmail.com>
2019-07-23 09:27:34 -0700

    Update requirements.sh

--- environment/shire/aws/scripts/HELK/requirements.sh
+++ environment/shire/aws/scripts/HELK/requirements.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-sudo apt-get -f install -y
 apt-get purge python3-pip -y
+apt-get purge python-pip -y
 apt-get autoremove -y
 apt-get purge python3.5 -y
+apt-get purge python3 -y
+apt-get -f install -y
 apt-get install python3.5 -y
-apt-get install python-pip -y
-pip install --upgrade pip 
-pip install pandas
-pip install tabulate
+apt-get install python3-pip -y
+pip3 install --upgrade pip
+pip3 install pandas
+pip3 install tabulate
