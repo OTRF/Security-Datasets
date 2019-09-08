6c1c31ca676702d17569984cff1820dfcc709222
cyb3rward0g <rrodriguezops@gmail.com>
2019-09-07 23:33:14 -0400

    Update Add-DomainAdmin.ps1
    Adding AD DS Server

--- environment/shire/aws/cloudformation/scripts/Add-DomainAdmin.ps1
+++ environment/shire/aws/cloudformation/scripts/Add-DomainAdmin.ps1
@@ -3,6 +3,7 @@
 
 # References:
 # https://aws.amazon.com/blogs/compute/optimizing-joining-windows-server-instances-to-a-domain-with-powershell-in-aws-cloudformation/
+# https://github.com/aws-quickstart/quickstart-microsoft-activedirectory/blob/master/scripts/archive/Create-AdminUser.ps1
 
 [CmdletBinding()]
 param(
@@ -13,22 +14,21 @@ param(
     [string]$DomainDNSName,
 
     [Parameter(Mandatory=$true, Position=2)]
-    [string]$DomainAdminPassword
+    [string]$DomainAdminPassword,
+
+    [Parameter(Mandatory=$true, Position=3)]
+    [string]$Server
 )
 
-# Verify if ADWS is still running. If not, it is started.
+# First Check: Verify if ADWS is still running. If not, start service.
 write-host "Verifying if ADWS is still running.."
 $s = Get-Service -Name ADWS
 while ($s.Status -ne 'Running'){
     Start-Service ADWS; Start-Sleep 3
 }
-Start-Sleep 60
+Start-Sleep 30
 
-# Add User
-write-host "Creating user $DomainAdminUser .."
-$u = New-ADUser -Name $DomainAdminUser -UserPrincipalName "$DomainAdminUser@$DomainDNSName" -AccountPassword (ConvertTo-SecureString $DomainAdminPassword -AsPlainText -Force) -Enabled $true -PasswordNeverExpires $true -PassThru
-
-# Add User to Domain Admins
+# Creating and Adding a user to Domain Admins
 $timeoutInSeconds = 300
 $elapsedSeconds = 0
 $intervalSeconds = 1
@@ -37,12 +37,21 @@ $running = $false
 
 While (($elapsedSeconds -lt $timeoutInSeconds )) {
     try {
-        $Groups = @('domain admins','schema admins','enterprise admins')
-        $Groups | ForEach-Object{
-            write-host "Adding user $DomainAdminUser to $_ .."
-            Add-ADGroupMember -Identity $_ -Members $DomainAdminUser
-        }
-        break         
+        # Second Check: Verify if the ADWS process is running
+        $adws = Get-Process -Name Microsoft.ActiveDirectory.WebServices
+        $UserPrincipalName = $DomainAdminUser+"@"+$DomainDNSName
+        $ADServer = $Server+"."+$DomainDNSName
+        if ($adws) {
+            write-host "Creating user $UserPrincipalName .."
+            New-ADUser -Name $DomainAdminUser -UserPrincipalName $UserPrincipalName -AccountPassword (ConvertTo-SecureString $DomainAdminPassword  -AsPlainText -Force) -Enabled $true -PasswordNeverExpires $true -Server $ADServer
+            write-host "Successfully Created $UserPrincipalName..."
+            $Groups = @('domain admins','schema admins','enterprise admins')
+            $Groups | ForEach-Object{
+                write-host "Adding user $DomainAdminUser to $_ .."
+                Add-ADGroupMember -Identity $_ -Members $DomainAdminUser
+            }
+            break
+        }           
     }
     catch {
         Start-Sleep -Seconds $elapsedSeconds
