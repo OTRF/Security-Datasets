6bf896c80f696679e9e37ba1a9a71f797a267899
cyb3rward0g <rrodriguezops@gmail.com>
2019-09-08 11:28:59 -0400

    Update Create-SecurityGroup.ps1

--- environment/shire/setup/scripts/Create-SecurityGroup.ps1
+++ environment/shire/setup/scripts/Create-SecurityGroup.ps1
@@ -16,5 +16,5 @@ $ParentPath = "DC=$DomainNetBIOSName,DC=com"
 
 write-host "Creating Security Group $SecurityGroup on $ParentPath .." 
 New-ADGroup -Name $SecurityGroup -GroupCategory Security -GroupScope Global `
-    -DisplayName $SecurityGroup -Path "CN=Users,$ParentPath" `
+    -DisplayName "$SecurityGroup" -Path "CN=Users,$ParentPath" `
     -Description "Security group $SecurityGroup" -PassThru
