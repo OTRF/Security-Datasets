877e23ea2f0e142b5563c3ec0fee63ea8478e4af
Cyb3rWard0g <rpberto@38f9d3724f84.ant.amazon.com>
2019-09-11 08:50:23 -0400

    Adding Internal Communications

--- environment/setup/scripts/Set-StaticIP.ps1
+++ environment/setup/scripts/Set-StaticIP.ps1
@@ -4,8 +4,21 @@
 # References:
 # https://aws.amazon.com/blogs/compute/optimizing-joining-windows-server-instances-to-a-domain-with-powershell-in-aws-cloudformation/
 
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$false)]
+    [string]$ServerAddresses
+)
+
 $netip = Get-NetIPConfiguration
 $ipconfig = Get-NetIPAddress | ?{$_.IpAddress -eq $netip.IPv4Address.IpAddress}
 Get-NetAdapter | Set-NetIPInterface -DHCP Disabled
 Get-NetAdapter | New-NetIPAddress -AddressFamily IPv4 -IPAddress $netip.IPv4Address.IpAddress -PrefixLength $ipconfig.PrefixLength -DefaultGateway $netip.IPv4DefaultGateway.NextHop
-Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses $netip.DNSServer.ServerAddresses
+if($ServerAddresses)
+{
+    Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses $ServerAddresses
+
+}
+else{
+    Get-NetAdapter | Set-DnsClientServerAddress -ServerAddresses $netip.DNSServer.ServerAddresses
+}
