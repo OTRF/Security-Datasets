008cf7d3d2711b1464aef976dbc6d5d287a45f7a
Cyb3rWard0g <rpberto@38f9d3724f84.ant.amazon.com>
2019-09-14 02:16:42 -0400

    Update Prepare-Box.ps1

--- environment/setup/scripts/Prepare-Box.ps1
+++ environment/setup/scripts/Prepare-Box.ps1
@@ -26,10 +26,6 @@ Write-Host "Allow ICMP Traffic through firewall"
 Write-Host "Enable File and Printer Sharing"
 & netsh firewall set service type = fileandprint mode = enable
 
-# Set Performance
-Write-Host "Setting Best Performance"
-new-itemproperty -path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects -name VisualFXSetting -propertyType DWord -value 2 -force
-
 # Power Settings
 Write-Host "Setting Power Performance"
 $HPGuid = (Get-WmiObject -Class win32_powerplan -Namespace root\cimv2\power -Filter "ElementName='High performance'").InstanceID.tostring()
