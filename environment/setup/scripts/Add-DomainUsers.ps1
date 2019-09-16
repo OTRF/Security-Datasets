fd21ef3b995ef4eb08b51bb7221ed3bae9a676ef
Cyb3rWard0g <rpberto@38f9d3724f84.ant.amazon.com>
2019-09-16 01:32:09 -0400

    Updated Users Setup

--- environment/setup/scripts/Add-DomainUsers.ps1
+++ environment/setup/scripts/Add-DomainUsers.ps1
@@ -13,7 +13,7 @@ param(
     [Parameter(Mandatory=$true, Position=1)]
     [string]$DomainDNSName,
 
-    [Parameter(Mandatory=$true, Position=3)]
+    [Parameter(Mandatory=$true, Position=2)]
     [string]$Server,
 
     [Parameter(Mandatory=$true, Position=3)]
@@ -27,84 +27,64 @@ while ($s.Status -ne 'Running'){
 }
 Start-Sleep 5
 
-$timeoutInSeconds = 300
-$elapsedSeconds = 0
-$intervalSeconds = 1
-$startTime = Get-Date
-$running = $false
+$ADServer = $Server+"."+$DomainDNSName
+$DomainName1,$DomainName2 = $DomainDNSName.split('.')
 
-While (($elapsedSeconds -lt $timeoutInSeconds )) {
-    try {
-        write-host "Verifying if ADWS is still running .."
-        $adws = Get-Process -Name Microsoft.ActiveDirectory.WebServices
+write-host "Importing domain users from $CSVFile .."
+Import-Csv $CSVFile | ForEach-Object {
+    $UserPrincipalName = $_.SamAccountName + "@" + $DomainDNSName
+    $DisplayName = $_.LastName + " " + $_.FirstName
+    $OUPath = "OU="+$_.UserContainer+",DC=$DomainName1,DC=$DomainName2"
+    $SamAccountName = $_.SamAccountName
+    $ServiceName = $_.FirstName
 
-        if ($adws) {
-            $ADServer = $Server+"."+$DomainDNSName
-            $DomainName1,$DomainName2 = $DomainDNSName.split('.')
-    
-            write-host "Importing domain users from $CSVFile .."
-            Import-Csv $CSVFile | ForEach-Object {
-                $UserPrincipalName = $_.SamAccountName + "@" + $DomainDNSName
-                $DisplayName = $_.LastName + " " + $_.FirstName
-                $OU = "CN="+$_.UserContainer+",DC=$DomainName1,DC=$DomainName2"
+    write-host "Checking if user $SamAccountName exists or not.."
 
-                write-host "Checking if user $UserPrincipalName exists or not.."
-                $User = Get-ADUser -LDAPFilter "(sAMAccountName=$DomainAdminUser)"
+    $User = Get-ADUser -LDAPFilter "(sAMAccountName=$SamAccountName)"
+    Write-Host $User
 
-                if ($User -eq $Null)
-                {
-                    write-host "Creating user $UserPrincipalName .."
-                    New-ADUser -Name $DisplayName `
-                    -DisplayName $DisplayName `
-                    -GivenName $_.FirstName `
-                    -Surname $_.LastName `
-                    -Department $_.Department `
-                    -Title $_.JobTitle `
-                    -UserPrincipalName $UserPrincipalName `
-                    -SamAccountName $_.SamAccountName `
-                    -Path $OU `
-                    -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -force) `
-                    -Enabled $true `
-                    -PasswordNeverExpires $true `
-                    -Server $ADServer
+    if ($User -eq $Null)
+    {
+        write-host "Creating user $UserPrincipalName .."
+        New-ADUser -Name $DisplayName `
+        -DisplayName $DisplayName `
+        -GivenName $_.FirstName `
+        -Surname $_.LastName `
+        -Department $_.Department `
+        -Title $_.JobTitle `
+        -UserPrincipalName $UserPrincipalName `
+        -SamAccountName $_.SamAccountName `
+        -Path $OUPath `
+        -AccountPassword (ConvertTo-SecureString $_.Password -AsPlainText -force) `
+        -Enabled $true `
+        -PasswordNeverExpires $true `
+        -Server $ADServer
 
-                    write-host "Successfully Created $UserPrincipalName..."
+        write-host "Successfully Created $UserPrincipalName..."
 
-                    if($_.Identity -Like "Domain Admins"){
+        if($_.Identity -Like "Domain Admins"){
 
-                        write-host "Adding user $UserPrincipalName to Domain Admin groups .."
-                        $DomainAdminUser = $_.SamAccountName
-                        $Groups = @('domain admins','schema admins','enterprise admins')
-                        $Groups | ForEach-Object{
-                            $members = Get-ADGroupMember -Identity $_ -Recursive | Select -ExpandProperty Name
-                            if ($members -contains $DomainAdminUser) {
-                                Write-Host "$DomainAdminUser exists in $_ "
-                            }
-                            else {
-                                Write-Host "$DomainAdminUser does not exists in group $_ "
-                                write-host "Adding user $DomainAdminUser to $_ .."
-                                Add-ADGroupMember -Identity $_ -Members $DomainAdminUser
-                            }
-                        }
-                    }
-                    if($_.JobTitle -Like "Service Account"){
-                        setspn -a $_.FirstName/$DomainDNSName $DomainName1\$_.SamAccountName
-                    }
+            write-host "Adding user $UserPrincipalName to Domain Admin groups .."
+            $DomainAdminUser = $_.SamAccountName
+            $Groups = @('domain admins','schema admins','enterprise admins')
+            $Groups | ForEach-Object{
+                $members = Get-ADGroupMember -Identity $_ -Recursive | Select -ExpandProperty Name
+                if ($members -contains $DomainAdminUser) {
+                    Write-Host "$DomainAdminUser exists in $_ "
+                }
+                else {
+                    Write-Host "$DomainAdminUser does not exists in group $_ "
+                    write-host "Adding user $DomainAdminUser to $_ .."
+                    Add-ADGroupMember -Identity $_ -Members $DomainAdminUser
                 }
             }
         }
-        else{
-            write-host "Starting ADWS Service again .."
-            Start-Service ADWS
-        }       
+        if($_.JobTitle -Like "Service Account"){
+            setspn -a $ServiceName/$DomainDNSName $DomainName1\$SamAccountName
+        }
     }
-    catch {
-        Start-Sleep -Seconds $elapsedSeconds
-        $elapsedSeconds = ($(Get-Date) - $startTime).TotalSeconds
-        echo "Elapse Seconds" $elapsedSeconds 
-        
-    }
-    if ($elapsedSeconds -ge $timeoutInSeconds) {
-        write-host "ADWS did not start or is unreachable in $timeoutInSeconds seconds..."
+    else
+    {
+        write-host "Account already exists.."
     }
 }
