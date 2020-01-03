8c09777cfa94426b9100ad243419c7f77256d986
Thomas Patzke <thomas@patzke.org>
2020-01-03 23:44:51 +0100

    Improved ES import
    * Support for datasets generated with Winlogbeat <7 and NXLog.

--- scripts/es-import.py
+++ scripts/es-import.py
@@ -62,7 +62,70 @@ with progressbar.DataTransferBar(max_value=total_size) as progress:
                 def generate_actions(f, progress):
                     for line in f:
                         source = json.loads(line)
-                        source["log"] = { "file": logfile }
+                        source["log"] = { "file": { "name": logfile }}
+                        source.setdefault("winlog", dict())
+
+                        # Plain data created by nxlog is completely moved to winlog.event_data except blacklisted
+                        if "EventID" in source:
+                            # Move event id to appropriate location
+                            source["winlog"]["event_id"] = source["EventID"]
+                            del source["EventID"]
+
+                            # Discard unneeded fields
+                            try:
+                                del source["type"]
+                            except KeyError:
+                                pass
+
+                            try:
+                                del source["host"]
+                            except KeyError:
+                                pass
+
+                            # Move fields from top level to winlog.event_data
+                            source["winlog"]["event_data"] = {
+                                        k: v
+                                        for k, v in source.items()
+                                        if k not in ("winlog", "Channel", "Hostname", "@timestamp", "@version")
+                                    }
+                            for k in source["winlog"]["event_data"].keys():
+                                del source[k]
+
+                            # Special handling for host name
+                            try:
+                                source["winlog"]["computer_name"] = source["Hostname"]
+                                del source["Hostname"]
+                            except KeyError:
+                                pass
+
+                            # Special handling for channel
+                            try:
+                                source["winlog"]["channel"] = source["Channel"]
+                                del source["Channel"]
+                            except KeyError:
+                                pass
+
+                        # Data created with Winlogbeat <7 contains event fields in event_data instead of winlog.event_data - move it
+                        if "event_data" in source:
+                            source["winlog"]["event_data"] = source["event_data"]
+                            del source["event_data"]
+                        # Old Winlogbeats also put the channel name in the log_name field move this to new field names
+                        if "log_name" in source:
+                            source["winlog"]["channel"] = source["log_name"]
+                            del source["log_name"]
+                        # Some log records contain the channel name "security" in small letters, fix this
+                        try:
+                            if source["winlog"]["channel"] == "security":
+                                source["winlog"]["channel"] = "Security"
+                        except KeyError:
+                            pass
+                        # Old Winlogbeats also put the event id in a different location, move it to the new one
+                        if "event_id" in source:
+                            source["winlog"]["event_id"] = source["event_id"]
+                            del source["event_id"]
+                        # Also set event.code to event id
+                        source.setdefault("event", dict())["code"] = source["winlog"]["event_id"]
+
                         progress.update(progress.value + len(line))
                         yield {
                                 "_index": index,
